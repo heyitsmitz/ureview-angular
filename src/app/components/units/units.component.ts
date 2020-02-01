34c98bf56612cbd024ec44c7ccd95daa508b6266
ChinHuan <chinhuan99@hotmail.com>
2020-02-01 20:13:56 +0800

    Change units list to 2 units per row

--- src/app/components/units/units.component.ts
+++ src/app/components/units/units.component.ts
@@ -8,12 +8,22 @@ import { UnitsListService } from 'src/app/services/units-list.service';
 })
 export class UnitsComponent implements OnInit {
 
+  private _k: number = 2;      // Number of unit cards in a row
   units;
+  reordered_units;
   
   constructor(private unitsListService: UnitsListService) { }
 
   ngOnInit() {
     this.units = this.unitsListService.getUnits();
+    this._reorder();
+  }
+
+  private _reorder() {
+    this.reordered_units = [];
+    for (let i = 0; i < this.units.length; i += this._k) {
+      this.reordered_units.push({ units: this.units.slice(i, i + this._k) });
+    }
   }
 
 }
