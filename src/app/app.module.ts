c8a2187a61717479e996f33a411a54764b4bef9d
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-02-05 07:19:30 +0800

    Fix small bug where the FormModule was imported twice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { SidebarModule } from 'ng-sidebar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
